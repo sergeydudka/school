dc733acfe4c545b79fa91ca99fa2fccb1063592b
Sergey <soulevil111@gmail.com>
2018-12-09 10:55:50 +0200

    update composer, fix login option request

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,16 +1,29 @@
 <?php
 return [
-    'adminEmail'     => 'admin@example.com',
-    'loginUrl'       => '/main/auth/login',
-    'logoutUrl'       => '/main/auth/logout',
-    'menu'       => '/main/menu',
-    'edition'        => [
-        'default'   => 'ru',
+    'adminEmail' => 'admin@example.com',
+    'loginUrl' => '/main/auth/login',
+    'logoutUrl' => '/main/auth/logout',
+    'menu' => '/main/menu',
+    'edition' => [
+        'default' => 'ru',
         'maxLength' => 2,
     ],
     'adminUserGroup' => 1,
     'pagination' => [
         'page' => 1,
         'perPage' => 20,
+        'autoRefreshInterval' => 60 * 1000,
+        'pageSizes' => [
+            10,
+            20,
+            50,
+            100,
+        ],
+    ],
+    'auth' => true,
+    'formats' => [
+        'dateTime' => 'Y-m-d H:i:s',
+        'time' => 'H:i:s',
+        'date' => 'Y-m-d',
     ]
 ];
